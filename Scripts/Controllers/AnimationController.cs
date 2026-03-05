0ab8583b3375aecc0793dd577336af7880442fbe
The Ferryman <ferrymanstudios@gmail.com>
2026-03-05 09:01:34 -0600

    Create AnimationController.cs

---
+++ Scripts/Controllers/AnimationController.cs
@@ -0,0 +1,41 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Core;
+
+namespace SoM.Controllers {
+public class AnimationController : Singleton<AnimationController> {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    public void InitializeController()
+    {
+        //
+    }
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
