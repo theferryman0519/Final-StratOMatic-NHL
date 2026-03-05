4cdf63fa04a7848b142ba958036f225158ed13b5
The Ferryman <ferrymanstudios@gmail.com>
2026-03-05 13:56:30 -0600

    Update SkatersController.cs

--- Scripts/Controllers/SkatersController.cs
+++ Scripts/Controllers/SkatersController.cs
@@ -10,15 +10,19 @@ using UnityEngine.SceneManagement;
 
 // Game Dependencies
 using SoM.Core;
+using SoM.Skaters;
 
 namespace SoM.Controllers {
 public class SkatersController : Singleton<SkatersController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Creation Elements")]
+    [SerializeField] private SkaterCreationZone _skaterCreation;
+    [SerializeField] private GoalieCreationZone _goalieCreation;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public List<SkaterData> FullSkaters = new();
+    public List<GoalieData> FullGoalies = new();
 #endregion
 #region -------------------- Private Variables --------------------
     
@@ -30,11 +34,23 @@ public class SkatersController : Singleton<SkatersController> {
     
 #endregion
 #region -------------------- Public Methods --------------------
-    public void InitializeController()
+    public async void InitializeController()
     {
-        //
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the controller.");
+
+        FullSkaters.Clear();
+        FullGoalies.Clear();
+
+        await _skaterCreation.CreateSkaters();
+        await _goalieCreation.CreateGoalies();
+
+        CoreController.Inst.LoadingStepCompleted();
     }
 #endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}#endregion
 #region -------------------- Private Methods --------------------
     
 #endregion
