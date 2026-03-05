ac53661064b45fabd02f023b61da0ab1a386736d
The Ferryman <ferrymanstudios@gmail.com>
2026-03-05 08:34:18 -0600

    Create METHOD_TEMPLATE.cs

---
+++ Scripts/METHOD_TEMPLATE.cs
@@ -0,0 +1,49 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+// using SoM.Ai;
+// using SoM.Animation;
+// using SoM.Audio;
+// using SoM.Controllers;
+// using SoM.Core;
+// using SoM.Gameplay;
+// using SoM.Multiplayer;
+// using SoM.Playoffs;
+// using SoM.Seasons;
+// using SoM.Skaters;
+// using SoM.Ui;
+// using SoM.Users;
+
+namespace TEMPLATE {
+public class METHOD_TEMPLATE : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
