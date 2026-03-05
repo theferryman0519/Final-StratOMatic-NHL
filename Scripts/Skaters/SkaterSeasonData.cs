fed935d8c8dca12f6e5d382daf964a81ae0212c4
The Ferryman <ferrymanstudios@gmail.com>
2026-03-05 13:17:47 -0600

    Create SkaterSeasonData.cs

---
+++ Scripts/Skaters/SkaterSeasonData.cs
@@ -0,0 +1,44 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Skaters {
+[System.Serializable]
+public class SkaterSeasonData {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    
+    public int GamesPlayed { get; set; }
+    public int Goals { get; set; }
+    public int Assists { get; set; }
+    public int Points { get; set; }
+    public int PenaltyMinutes { get; set; }
+    public int PlusMinus { get; set; }
+    public int Shots { get; set; }
+    public int PowerplayGoals { get; set; }
+    public int PowerplayAssists { get; set; }
+    public int PowerplayPoints { get; set; }
+    public int ShorthandedGoals { get; set; }
+    public int ShorthandedAssists { get; set; }
+    public int ShorthandedPoints { get; set; }
+    public int Hits { get; set; }
+    public int BlockedShots { get; set; }
+    public int Giveaways { get; set; }
+    public int Takeaways { get; set; }
+    public int FaceoffsWon { get; set; }
+    public int FaceoffsLost { get; set; }
+    public int TimeOnIce { get; set; } // In Seconds
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
