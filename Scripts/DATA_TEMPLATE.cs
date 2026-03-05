3b1c4ea6bef7b431bf633abd10d62492e8535d9a
The Ferryman <ferrymanstudios@gmail.com>
2026-03-05 08:33:39 -0600

    Create DATA_TEMPLATE.cs

---
+++ Scripts/DATA_TEMPLATE.cs
@@ -0,0 +1,34 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+// using SoM.Ai;
+// using SoM.Animation;
+// using SoM.Audio;
+// using SoM.Controllers;
+// using SoM.Core;
+// using SoM.Gameplay;
+// using SoM.Multiplayer;
+// using SoM.Playoffs;
+// using SoM.Seasons;
+// using SoM.Skaters;
+// using SoM.Ui;
+// using SoM.Users;
+
+namespace TEMPLATE {
+public class DATA_TEMPLATE : MonoBehaviour {
+    
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
