b3163c88a8eb945dcc9be64aa5a47858a9d54c3f
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:41:03 -0500

    Create GoalieCard.cs

---
+++ Scripts/Models/Goalies/GoalieCard.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class GoalieCard {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string Penalty { get; set; }
+    public string Fatigue { get; set; }
+
+    public List<string> GoalieRatingActions = new();
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
