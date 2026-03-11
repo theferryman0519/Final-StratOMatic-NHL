cfb0d2b45144a6765650951c6a25648429efb41a
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 16:27:46 -0500

    Create UserSeasonStats.cs

---
+++ Scripts/Models/Users/UserSeasonStats.cs
@@ -0,0 +1,30 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class UserSeasonStats {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    
+    public int CurrentWins { get; set; }
+    public int CurrentLosses { get; set; }
+    public int CurrentTies { get; set; }
+    public int CurrentOTLs { get; set; }
+    public int TotalCups { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
