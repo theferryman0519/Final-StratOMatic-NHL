0a5e36159325840751e1395211a2ba83398cfb6a
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:36:31 -0500

    Create SkaterInfo.cs

---
+++ Scripts/Models/Skaters/SkaterInfo.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class SkaterInfo {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    public string Team { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
