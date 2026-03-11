c7d0971d63757b5198107c6d65cd271be721fd40
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:06:18 -0500

    Update UiController.cs

--- Scripts/Controllers/UiController.cs
+++ Scripts/Controllers/UiController.cs
@@ -15,16 +15,33 @@ namespace SoM.Controllers {
 public class UiController : Singleton<UiController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Panel Elements")]
+    [SerializeField] private UiBottomPanel _bottomPanel;
+
+    [Header("UI Elements")]
+    [SerializeField] private List<CanvasGroup> _backgroundElement;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public bool IsFadingBannerIn = false;
+    public bool IsFadingBannerOut = false;
 #endregion
 #region -------------------- Private Variables --------------------
     
 #endregion
 #region -------------------- Initial Functions --------------------
-    
+    void Start()
+    {
+        _bottomPanel.CanvasGroup.alpha = 0f;
+        _bottomPanel.gameObject.SetActive(false);
+        
+        AnimationController.Inst.FadeOutObjects(_backgroundElement, () =>
+        {
+	        foreach (CanvasGroup obj in _backgroundElement)
+	        {
+		        obj.gameObject.SetActive(false);
+	        }
+        });
+    }
 #endregion
 #region -------------------- Coroutines --------------------
     
@@ -33,7 +50,27 @@ public class UiController : Singleton<UiController> {
     public void InitializeController()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the controller.");
+
+        CoreController.Inst.LoadingStepCompleted();
     }
+
+    public void ShowBottomPanel(UiBottomPanelBase panelBase)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing the bottom panel.");
+        
+        _bottomPanel.gameObject.SetActive(true);
+        _bottomPanel.InitializePanel(panelBase);
+		_bottomPanel.CanvasGroup.alpha = 0f;
+
+		AnimationController.Inst.FadeInPanel(_bottomPanel.CanvasGroup, _bottomPanel.MainPanel);
+    }
+
+	public void CloseBottomPanel(Action continueAction = null)
+	{
+		CoreController.Inst.WriteLog(this.GetType().Name, $"Closing the bottom panel.");
+		
+		_bottomPanel.ClosePanel(continueAction);
+	}
 #endregion
 #region -------------------- Private Methods --------------------
     
