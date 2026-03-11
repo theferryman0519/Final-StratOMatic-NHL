2de6c67d7f2004cc0764c6ee5b572d6e4a4f0c0e
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:07:16 -0500

    Update AnimationController.cs

--- Scripts/Controllers/AnimationController.cs
+++ Scripts/Controllers/AnimationController.cs
@@ -64,6 +64,20 @@ public class AnimationController : Singleton<AnimationController> {
         FadeObjects(elements, false, continueAction);
     }
 
+    public void FadeInPanel(CanvasGroup mainPanel, RectTransform panel, Action continueAction = null)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Fading the panel in.");
+
+        FadePanel(mainPanel, panel, true, continueAction);
+    }
+
+    public void FadeOutPanel(CanvasGroup mainPanel, RectTransform panel, Action continueAction = null)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Fading the panel out.");
+
+        FadePanel(mainPanel, panel, false, continueAction);
+    }
+
     public void ShrinkButton(Button mainButton, Action continueAction = null)
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Shrinking the button.");
@@ -88,6 +102,36 @@ public class AnimationController : Singleton<AnimationController> {
             element.alpha = initialAlpha;
             seq.Join(element.DOFade(finalAlpha, ConstantController.Fading_Multiplier));
         }
+
+        seq.OnComplete(() =>
+        {
+            continueAction?.Invoke();
+        });
+
+        seq.Play();
+    }
+
+    private void FadePanel(CanvasGroup mainPanel, RectTransform panel, bool isFadingIn, Action continueAction)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Fading the panel.");
+
+        float finalYPos = (isFadingIn) ? panel.anchoredPosition.y : panel.anchoredPosition.y * -2f;
+        float initialAlpha = (isFadingIn) ? 0f : 1f;
+        float finalAlpha = (isFadingIn) ? 1f : 0f;
+
+        if (isFadingIn)
+        {
+            panel.anchoredPosition = new Vector2(panel.anchoredPosition.x, panel.anchoredPosition.y * -2f);
+        }
+        
+        Sequence seq = DOTween.Sequence();
+
+        mainPanel.alpha = initialAlpha;
+        seq.Join(mainPanel.DOFade(finalAlpha, ConstantController.Fading_Multiplier));
+
+        Vector2 finalPos = new Vector2(panel.anchoredPosition.x, finalYPos);
+
+        seq.Join(panel.DOAnchorPos(finalPos, ConstantController.Fading_Multiplier));
 
         seq.OnComplete(() =>
         {
