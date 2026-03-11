bbb013cf10cf528e2ea3ac749d41dcff9524881e
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:51:22 -0500

    Create Team.cs

---
+++ Scripts/Models/Teams/Team.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class Team {
+    
+#region -------------------- Public Variables --------------------
+    public string Id;
+
+    public TeamInfo Info;
+    public TeamGame Game;
+    public TeamSeason Season;
+    public TeamPlayoff Playoff;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
