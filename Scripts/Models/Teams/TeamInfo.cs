0dbad8a1d641bf87396aa09bd5ea687c05bb3584
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:59:18 -0500

    Create TeamInfo.cs

---
+++ Scripts/Models/Teams/TeamInfo.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class TeamInfo {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string Code { get; set; }
+    public string CityName { get; set; }
+    public string NickName { get; set; }
+    public string League { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
