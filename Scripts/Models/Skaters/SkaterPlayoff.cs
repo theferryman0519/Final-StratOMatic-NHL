635b728259490bc7b67df79d939c87e5c27bef07
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:31:47 -0500

    Create SkaterPlayoff.cs

---
+++ Scripts/Models/Skaters/SkaterPlayoff.cs
@@ -0,0 +1,42 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class SkaterPlayoff {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+
+    public int GamesPlayed { get; set; }
+    public int Goals { get; set; }
+    public int Assists { get; set; }
+    public int Points { get; set; }
+    public int PlusMinus { get; set; }
+    public int PenaltyMinutes { get; set; }
+    public int PowerplayGoals { get; set; }
+    public int PowerplayAssists { get; set; }
+    public int PowerplayPoints { get; set; }
+    public int ShorthandedGoals { get; set; }
+    public int ShorthandedAssists { get; set; }
+    public int ShorthandedPoints { get; set; }
+    public int Shots { get; set; }
+    public int Giveaways { get; set; }
+    public int Takeaways { get; set; }
+    public int FaceoffsWon { get; set; }
+    public int FaceoffsLost { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
