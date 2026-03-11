a7a4d35ecd5bcb7d7e7277c27d3e544a974ff323
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 16:05:06 -0500

    Create TeamSeason.cs

---
+++ Scripts/Models/Teams/TeamSeason.cs
@@ -0,0 +1,42 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class TeamSeason {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    
+    public int GamesPlayed { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int Ties { get; set; }
+    public int OTLs { get; set; }
+    public int Points { get; set; }
+    public int Goals { get; set; }
+    public int Shots { get; set; }
+    public int PowerplayGoals { get; set; }
+    public int Powerplays { get; set; }
+    public int ShorthandedGoals { get; set; }
+    public int FaceoffsWon { get; set; }
+    public int FaceoffsLost { get; set; }
+    public int Hits { get; set; }
+    public int BlockedShots { get; set; }
+    public int Giveaways { get; set; }
+    public int Takeaways { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
