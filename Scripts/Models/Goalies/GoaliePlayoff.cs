c9f96b379937c1fc10283e27aa4c539c6a050a4e
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:43:28 -0500

    Create GoaliePlayoff.cs

---
+++ Scripts/Models/Goalies/GoaliePlayoff.cs
@@ -0,0 +1,33 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class GoaliePlayoff {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    
+    public int GamesPlayed { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int Ties { get; set; }
+    public int GoalsAgainst { get; set; }
+    public int ShotsAgainst { get; set; }
+    public int Assists { get; set; }
+    public int PenaltyMinutes { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
