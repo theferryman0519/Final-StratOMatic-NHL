575d529bf622a3f3d9639435a358177a2ef8f0a3
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 15:42:21 -0500

    Create GoalieGame.cs

---
+++ Scripts/Models/Goalies/GoalieGame.cs
@@ -0,0 +1,29 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class GoalieGame {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    
+    public int GoalsAgainst { get; set; }
+    public int ShotsAgainst { get; set; }
+    public int Assists { get; set; }
+    public int PenaltyMinutes { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
