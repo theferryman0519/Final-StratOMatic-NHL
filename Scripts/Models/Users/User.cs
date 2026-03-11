7ee12d0232c06c6987602db95b7ef1a28bb65b40
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 16:35:02 -0500

    Create User.cs

---
+++ Scripts/Models/Users/User.cs
@@ -0,0 +1,27 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class User {
+    
+#region -------------------- Public Variables --------------------
+    public string Id;
+    
+    public UserInfo Info;
+    public UserStats Stats;
+    public UserSeasonStats SeasonStats;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
