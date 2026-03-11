dc7049ef5d7fec61a10f8ab214031985b28929f3
The Ferryman <ferrymanstudios@gmail.com>
2026-03-11 16:14:19 -0500

    Create UserInfo.cs

---
+++ Scripts/Models/Users/UserInfo.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class UserInfo {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Team { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
