f7e194eaf897e0e8e710f75ba8e9a6c0deac12de
The Ferryman <ferrymanstudios@gmail.com>
2026-03-12 08:58:09 -0500

    Create UiSceneBase.cs

---
+++ Scripts/Ui/Scenes/Base/UiSceneBase.cs
@@ -0,0 +1,94 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Controllers;
+
+namespace SoM.Ui {
+public class UiSceneBase : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Banner Elements")]
+    [SerializeField] protected Image _bannerBackground;
+    [SerializeField] protected Image _bannerLogo;
+    [SerializeField] protected TMP_Text _bannerTitle;
+
+    [Header("Canvas Group Elements")]
+    [SerializeField] protected CanvasGroup _banner;
+    [SerializeField] protected List<CanvasGroup> _mainContent;
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    protected virtual InitializeUi()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the UI for the scene.");
+
+        List<CanvasGroup> fadeInElements = new();
+
+		foreach (CanvasGroup mainContent in _mainContent)
+		{
+			fadeInElements.Add(mainContent);
+		}
+
+		if (UiController.Inst.IsFadingBannerIn)
+		{
+			fadeInElements.Add(_banner.CanvasGroup);
+		}
+
+        AnimationController.Inst.FadeInObjects(fadeInElements, () =>
+		{
+			UiController.Inst.IsFadingBannerIn = false;
+		});
+    }
+
+    public void GoToNewScene(string sceneName)
+	{
+		CoreController.Inst.WriteLog(this.GetType().Name, $"Going to a new scene: {sceneName}.");
+
+		List<CanvasGroup> fadeOutElements = new();
+
+		foreach (CanvasGroup mainContent in _mainContent)
+		{
+			fadeOutElements.Add(mainContent);
+		}
+
+		if (UiController.Inst.IsFadingBannerOut)
+		{
+			fadeOutElements.Add(_banner.CanvasGroup);
+		}
+
+		ContinueToScene(sceneName, finalElements);
+	}
+#endregion
+#region -------------------- Private Methods --------------------
+    private void ContinueToScene(string sceneName, List<CanvasGroup> elements)
+	{
+		CoreController.Inst.WriteLog(this.GetType().Name, $"Continuing to scene: {sceneName}.");
+
+		AnimationController.Inst.FadeOutObjects(elements, () =>
+		{
+			UiController.Inst.IsFadingBannerIn = false;
+
+			CoreController.Inst.ChangeScene(sceneName);
+		});
+	}
+#endregion
+}}
