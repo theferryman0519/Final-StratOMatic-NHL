ff5342223223ee05f5ac6d509e356984f4268777
The Ferryman <ferrymanstudios@gmail.com>
2026-03-12 09:37:27 -0500

    Create FirebaseLogin.cs

---
+++ Scripts/Models/Firebase/FirebaseLogin.cs
@@ -0,0 +1,26 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class FirebaseLogin {
+    
+#region -------------------- Public Variables --------------------
+    public string Email;
+    public string Password;
+    public Action SuccessAction;
+    public Action NoAccountAction;
+    public Action NoDataAction;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
