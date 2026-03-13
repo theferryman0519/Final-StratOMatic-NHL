2529237d605bc29dcd5e65378d801b538f55c435
The Ferryman <ferrymanstudios@gmail.com>
2026-03-13 11:02:45 -0500

    Create FirebaseRest.cs

---
+++ Scripts/Models/Firebase/FirebaseRest.cs
@@ -0,0 +1,26 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class FirebaseRest {
+    
+#region -------------------- Public Variables --------------------
+    public string Url;
+    public string Method;
+    public string Json;
+    public Action<string> SuccessAction;
+    public Action<string> FailAction;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
