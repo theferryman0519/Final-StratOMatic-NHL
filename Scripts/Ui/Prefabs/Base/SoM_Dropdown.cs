c8ac53933c4c052af67b467d79365a22fd2d2d93
The Ferryman <ferrymanstudios@gmail.com>
2026-03-16 12:04:10 -0500

    Create SoM_Dropdown.cs

---
+++ Scripts/Ui/Prefabs/Base/SoM_Dropdown.cs
@@ -0,0 +1,49 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Controllers;
+
+namespace SoM.Ui {
+public class SoM_Dropdown : MonoBehavior {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Input Field Elements")]
+    [SerializeField] private TMP_Dropdown _dropdown;
+#endregion
+#region -------------------- Public Variables --------------------
+    public TMP_Dropdown Dropdown => _dropdown;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    public void SetListeners(Action changeAction)
+    {
+        _dropdown.onValueChanged.RemoveAllListeners();
+
+        _dropdown.onValueChanged.AddListener(changeAction);
+    }
+
+    public string GetValue()
+    {
+        return _dropdown.value;
+    }
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
