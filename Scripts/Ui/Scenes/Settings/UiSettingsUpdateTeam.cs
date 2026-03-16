928ff8e8983aee1216c0ef39a456f04e9f73c749
The Ferryman <ferrymanstudios@gmail.com>
2026-03-16 13:11:20 -0500

    Create UiSettingsUpdateTeam.cs

---
+++ Scripts/Ui/Scenes/Settings/UiSettingsUpdateTeam.cs
@@ -0,0 +1,104 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Controllers;
+using SoM.Models;
+
+namespace SoM.Ui {
+public class UiSettingsUpdateTeam : UiSceneBase {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Grid Elements")]
+    [SerializeField] private Transform _container;
+    [SerializeField] private FavoriteTeamPrefab _teamPrefab;
+
+    [Header("Button Elements")]
+    [SerializeField] private SoM_Button _saveButton;
+
+    [Header("Team Select Elements")]
+    [SerializeField] private TMP_Text _selectionText;
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    private Team favTeam;
+#endregion
+#region -------------------- Initial Functions --------------------
+    void Start()
+    {
+        InitializeUi();
+    }
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    protected override void InitializeUi()
+	{
+        _saveButton.SetListener(SaveChanges);
+
+        SetContainer();
+
+        base.InitializeUi();
+	}
+#endregion
+#region -------------------- Private Methods --------------------
+    private void SaveChanges()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Attempting to save changes to the user favorite team.");
+
+        // TODO
+
+		GoToScene(CoreController.Inst.Scene_Settings02);
+    }
+    
+    private void SetContainer()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Setting the favorite team container.");
+
+        ClearContainer();
+
+        foreach (Team team in TeamsController.Inst.AllTeams)
+        {
+            GameObject icon = Instantiate(_teamPrefab, _container);
+
+            icon.SetIcon(team, false);
+            icon.SetListener(() =>
+            {
+                CoreController.Inst.WriteLog(this.GetType().Name, $"Choosing {team.Info.Code} as a favorite team.");
+
+                favTeam = team;
+                icon.SetIcon(team, true);
+
+                _selectionText.text = $"You have selected the {team.Info.CityName} {team.Info.NickName} of the {team.Info.League}";
+                _signUpButton.gameObject.SetActive(true);
+            });
+        }
+    }
+
+    private void ClearContainer()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Clearing the favorite team container.");
+
+        foreach (Transform child in _container)
+        {
+            Destroy(child.gameObject);
+        }
+
+        _selectionText.text = "You have not selected a team";
+
+        favTeam = null;
+
+        _signUpButton.gameObject.SetActive(false);
+    }
+#endregion
+}}
