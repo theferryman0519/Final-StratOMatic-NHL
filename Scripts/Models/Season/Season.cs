b02749e0f982aac34ba8432e704a2e741c74c5a1
The Ferryman <ferrymanstudios@gmail.com>
2026-03-18 09:59:14 -0500

    Create Season.cs

---
+++ Scripts/Models/Season/Season.cs
@@ -0,0 +1,30 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class Season {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string League { get; set; }
+
+    public int Version { get; set; }
+
+    public Team Team { get; set; }
+
+    public List<GameNight> GameNights { get; set; } = new();
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
