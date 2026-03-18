340dfafce6b7ddaed7dc369b5bb80b61c940e9c2
The Ferryman <ferrymanstudios@gmail.com>
2026-03-18 09:44:43 -0500

    Create Game.cs

---
+++ Scripts/Models/Game/Game.cs
@@ -0,0 +1,30 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class Game {
+    
+#region -------------------- Public Variables --------------------
+    public string Id;
+    public string Type;
+
+    public User HomeUser;
+    public User AwayUser;
+
+    public GameStats HomeStats;
+    public GameStats AwayStats;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
