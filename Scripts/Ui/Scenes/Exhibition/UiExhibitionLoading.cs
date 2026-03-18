a53a06f6d3dcdcb2ad1d27b5317172ba9967ad07
The Ferryman <ferrymanstudios@gmail.com>
2026-03-18 09:20:39 -0500

    Create UiExhibitionLoading.cs

---
+++ Scripts/Ui/Scenes/Exhibition/UiExhibitionLoading.cs
@@ -0,0 +1,64 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Controllers;
+using SoM.Models;
+
+namespace SoM.Ui {
+public class UiExhibitionLoading : UiSceneBase {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Icon Elements")]
+    [SerializeField] private Image _homeIcon;
+    [SerializeField] private Image _awayIcon;
+
+    [Header("Loading Elements")]
+	[SerializeField] private Slider _loadingBar;
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    void Start()
+    {
+        InitializeUi();
+    }
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    protected override void InitializeUi()
+	{
+		SetGameData();
+
+        base.InitializeUi(StartLoading);
+	}
+#endregion
+#region -------------------- Private Methods --------------------
+    private void SetGameData()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Setting the game data.");
+
+        // TODO
+    }
+
+    private void StartLoading()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Starting to load the exhibition game.");
+
+        // TODO
+    }
+#endregion
+}}
