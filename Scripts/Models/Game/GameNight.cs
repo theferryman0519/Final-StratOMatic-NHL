a6e9d0abb323a8a25f9d29ef9b803fb49147ab96
The Ferryman <ferrymanstudios@gmail.com>
2026-03-18 10:03:27 -0500

    Create GameNight.cs

---
+++ Scripts/Models/Game/GameNight.cs
@@ -0,0 +1,25 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class GameNight {
+    
+#region -------------------- Public Variables --------------------
+    public int Number { get; set; }
+
+    public List<Game> Games { get; set; } = new();
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
