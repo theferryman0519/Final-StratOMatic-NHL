21aae003336e98d7c7083a1a642ab3254d512030
The Ferryman <ferrymanstudios@gmail.com>
2026-03-23 13:16:47 -0500

    Create TeamDatabase.cs

---
+++ Scripts/Models/Teams/TeamDatabase.cs
@@ -0,0 +1,27 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class TeamDatabase {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string InfoString { get; set; }
+    public string SeasonString { get; set; }
+    public string PlayoffString { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
