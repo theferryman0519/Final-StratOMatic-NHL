968f75f8c5e5d6e024e23de554c6e3aa491667c0
The Ferryman <ferrymanstudios@gmail.com>
2026-03-24 13:30:48 -0500

    Create UserDatabase.cs

---
+++ Scripts/Models/Users/UserDatabase.cs
@@ -0,0 +1,27 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+[System.Serializable]
+public class UserDatabase {
+    
+#region -------------------- Public Variables --------------------
+    public string Id { get; set; }
+    public string InfoString { get; set; }
+    public string StatsString { get; set; }
+    public string SeasonStatsString { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
