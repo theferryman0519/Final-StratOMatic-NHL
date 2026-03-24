41b2785672a811c5672b523947753dd371faa134
The Ferryman <ferrymanstudios@gmail.com>
2026-03-24 09:16:10 -0500

    Create BottomPanel.cs

---
+++ Scripts/Models/Panels/BottomPanel.cs
@@ -0,0 +1,32 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class BottomPanel {
+    
+#region -------------------- Public Variables --------------------
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string ButtonA { get; set; }
+    public string ButtonB { get; set; }
+
+    public bool HasCloseButton { get; set; }
+
+    public int ButtonCount { get; set; }
+
+    public Action ActionA { get; set; }
+    public Action ActionB { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
