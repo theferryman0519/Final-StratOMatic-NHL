94dc9458f8cc55a890653bf80e746cb912ffa6b1
The Ferryman <ferrymanstudios@gmail.com>
2026-03-25 15:19:09 -0500

    Update GoaliesController.cs

--- Scripts/Controllers/GoaliesController.cs
+++ Scripts/Controllers/GoaliesController.cs
@@ -20,7 +20,10 @@ public class GoaliesController : Singleton<GoaliesController> {
     [SerializeField] private GoalieCreation _goalieCreation;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public Dictionary<string, List<string>> NhlGoalies = new();
+    public Dictionary<string, List<string>> PwhlGoalies = new();
+    public Dictionary<string, List<string>> NhlFranchiseGoalies = new();
+    public Dictionary<string, List<string>> PwhlFranchiseGoalies = new();
 #endregion
 #region -------------------- Private Variables --------------------
     
@@ -35,9 +38,65 @@ public class GoaliesController : Singleton<GoaliesController> {
     public void InitializeController()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the controller.");
+
+        NhlGoalies.Clear();
+        PwhlGoalies.Clear();
+        NhlFranchiseGoalies.Clear();
+        PwhlFranchiseGoalies.Clear();
+
+        SetAllGoalies();
     }
 #endregion
 #region -------------------- Private Methods --------------------
-    
+    private async void SetAllGoalies()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Setting all goalies.");
+
+        await FirebaseController.Inst.GetAllGoalies(allGoaliesData =>
+        {
+            foreach (GoalieDatabase goalieData in allGoaliesData)
+            {
+                Goalie goalie = await _GoalieCreation.CreateGoalie(goalieData);
+
+                switch (goalie.Info.League)
+                {
+                    case "NHL":
+                        if (!NhlGoalies.ContainsKey(goalie.Info.Team))
+                        {
+                            NhlGoalies.Add(goalie.Info.Team, new());
+                        }
+
+                        NhlGoalies[goalie.Info.Team].Add(goalie);
+                        break;
+                    case "PWHL":
+                        if (!PwhlGoalies.ContainsKey(goalie.Info.Team))
+                        {
+                            PwhlGoalies.Add(goalie.Info.Team, new());
+                        }
+
+                        PwhlGoalies[goalie.Info.Team].Add(goalie);
+                        break;
+                    case "NHL-Franchise":
+                        if (!NhlFranchiseGoalies.ContainsKey(goalie.Info.Team))
+                        {
+                            NhlFranchiseGoalies.Add(goalie.Info.Team, new());
+                        }
+
+                        NhlFranchiseGoalies[goalie.Info.Team].Add(goalie);
+                        break;
+                    case "PWHL-Franchise":
+                        if (!PwhlFranchiseGoalies.ContainsKey(goalie.Info.Team))
+                        {
+                            PwhlFranchiseGoalies.Add(goalie.Info.Team, new());
+                        }
+
+                        PwhlFranchiseGoalies[goalie.Info.Team].Add(goalie);
+                        break;
+                }
+            }
+
+            CoreController.Inst.LoadingStepCompleted();
+        });
+    }
 #endregion
 }}
