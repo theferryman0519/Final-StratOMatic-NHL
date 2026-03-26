e238f474c805c041c0f49fa2ab89c8b464d24086
The Ferryman <ferrymanstudios@gmail.com>
2026-03-26 11:13:13 -0500

    Update UiSceneBase.cs

--- Scripts/Ui/Scenes/Base/UiSceneBase.cs
+++ Scripts/Ui/Scenes/Base/UiSceneBase.cs
@@ -41,6 +41,8 @@ public class UiSceneBase : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the UI for the scene.");
 
+		SetBanner();
+
         List<CanvasGroup> fadeInElements = new();
 
 		foreach (CanvasGroup mainContent in _mainContent)
@@ -60,11 +62,6 @@ public class UiSceneBase : MonoBehaviour {
 			continueAction?.Invoke();
 		});
     }
-
-	public void SetBanner(Team team)
-	{
-		// TODO
-	}
 
     public void GoToNewScene(string sceneName)
 	{
@@ -96,6 +93,69 @@ public class UiSceneBase : MonoBehaviour {
 
 			CoreController.Inst.ChangeScene(sceneName);
 		});
+	}
+
+	private void SetBanner()
+	{
+		CoreController.Inst.WriteLog(this.GetType().Name, $"Setting the main banner.");
+
+		if (SeasonsController.Inst.SeasonData != null)
+		{
+			string spriteName = $"{SeasonsController.Inst.SeasonData.League}_{SeasonsController.Inst.SeasonData.Team.Info.Code}";
+
+			_bannerBackground.sprite = ConstantController.Inst.BannerSprites[spriteName];
+			_bannerLogo.sprite = ConstantController.Inst.LogoSprites[spriteName];
+
+			SetBannerTitle();
+
+			return;
+		}
+
+		if (PlayoffsController.Inst.PlayoffData != null)
+		{
+			string spriteName = $"{PlayoffsController.Inst.PlayoffData.League}_{PlayoffsController.Inst.PlayoffData.Team.Info.Code}";
+
+			_bannerBackground.sprite = ConstantController.Inst.BannerSprites[spriteName];
+			_bannerLogo.sprite = ConstantController.Inst.LogoSprites[spriteName];
+
+			SetBannerTitle();
+
+			return;
+		}
+
+		if (GameplayController.Inst.GameData != null)
+		{
+			string spriteName = $"{GameplayController.Inst.GameData.HomeTeam.Info.League}_{GameplayController.Inst.GameData.HomeTeam.Info.Code}";
+
+			_bannerBackground.sprite = ConstantController.Inst.BannerSprites[spriteName];
+			_bannerLogo.sprite = ConstantController.Inst.LogoSprites[spriteName];
+
+			SetBannerTitle();
+
+			return;
+		}
+
+		Team userTeam = TeamsController.Inst.GetTeamFromCode(UsersController.Inst.UserData.Info.Team);
+		string spriteName = $"{userTeam.Info.League}_{userTeam.Info.Code}";
+
+		_bannerBackground.sprite = ConstantController.Inst.BannerSprites[spriteName];
+		_bannerLogo.sprite = ConstantController.Inst.LogoSprites[spriteName];
+
+		SetBannerTitle();
+	}
+
+	private void SetBannerTitle()
+	{
+		CoreController.Inst.WriteLog(this.GetType().Name, $"Setting the main banner title.");
+		
+		string sceneName = CoreController.Inst.GetSceneName();
+
+		if (sceneName.Contains("Settings")) { _bannerTitle.text = "Settings"; }
+		else if (sceneName.Contains("Exhibition")) { _bannerTitle.text = "Exhibition"; }
+		else if (sceneName.Contains("Season")) { _bannerTitle.text = "Season"; }
+		else if (sceneName.Contains("Playoff")) { _bannerTitle.text = "Playoff"; }
+		else if (sceneName.Contains("Multiplayer")) { _bannerTitle.text = "Multiplayer"; }
+		else { _bannerTitle.text = string.Empty; }
 	}
 #endregion
 }}
