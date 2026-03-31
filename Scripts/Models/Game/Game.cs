e63f890bd88fff777e68b6276ce6dd8573719ef4
The Ferryman <ferrymanstudios@gmail.com>
2026-03-31 14:20:00 -0500

    Update Game.cs

--- Scripts/Models/Game/Game.cs
+++ Scripts/Models/Game/Game.cs
@@ -17,11 +17,11 @@ public class Game {
 #region -------------------- Public Variables --------------------
     public string Id { get; set; }
     public string Type { get; set; }
-    public string HomeUserType { get; set; }
-    public string AwayUserType { get; set; }
+    public string HomeUserType { get; set; } // User, Ai
+    public string AwayUserType { get; set; } // User, Ai
 
-    public Team HomeTeam { get; set; }
-    public Team AwayTeam { get; set; }
+    public GameTeam HomeTeam { get; set; }
+    public GameTeam AwayTeam { get; set; }
 #endregion
 #region -------------------- Private Variables --------------------
     
