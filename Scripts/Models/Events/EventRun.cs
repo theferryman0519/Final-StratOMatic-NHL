adb5b67aaae82380d34224917ecc101cdad49124
The Ferryman <ferrymanstudios@gmail.com>
2026-03-31 15:08:14 -0500

    Update EventRun.cs

--- Scripts/Models/Events/EventRun.cs
+++ Scripts/Models/Events/EventRun.cs
@@ -16,7 +16,6 @@ public class EventRun {
 #region -------------------- Public Variables --------------------
     public string InfoText { get; set; }
     public string ActionText { get; set; }
-    public string PossPos { get; set; }
 #endregion
 #region -------------------- Private Variables --------------------
     
