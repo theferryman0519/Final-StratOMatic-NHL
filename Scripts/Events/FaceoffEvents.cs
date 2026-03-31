69b6af47d94c15a7e117c0a6dfab7fcdb56c7635
The Ferryman <ferrymanstudios@gmail.com>
2026-03-31 16:50:51 -0500

    Update FaceoffEvents.cs

--- Scripts/Events/FaceoffEvents.cs
+++ Scripts/Events/FaceoffEvents.cs
@@ -32,13 +32,16 @@ public class FaceoffEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding PuckDrop to the queue.");
 
-        // TODO
+        Skater homeSkaterMain = GetFaceoffCenter(true);
+        Skater awaySkaterMain = GetFaceoffCenter(false);
+
+        string homeSkater = $"{homeSkaterMain.Info.FirstName} {homeSkaterMain.Info.LastName}";
+        string awaySkater = $"{awaySkaterMain.Info.FirstName} {awaySkaterMain.Info.LastName}";
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = string.Empty,
+            InfoText = $"Each time a new segment of play occurs, that segment in initiated by a faceoff. The puck is dropped between two centers to start possession.",
+            ActionText = $"The puck is about to drop between {homeSkater} and {awaySkater}.",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -50,16 +53,18 @@ public class FaceoffEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding FaceoffStart to the queue.");
 
-        // TODO
+        Skater homeSkaterMain = GetFaceoffCenter(true);
+        Skater awaySkaterMain = GetFaceoffCenter(false);
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = string.Empty,
+            InfoText = $"Both centers line up for the faceoff. Each player has a Faceoff rating between 0 and 3. The higher the rating, the more often that center will win the faceoff.",
+            ActionText = $"Let's see who wins this faceoff as {homeSkaterMain.Info.LastName} has a Faceoff rating of +{homeSkaterMain.Card.Faceoff} while {awaySkaterMain.Info.LastName} has a Faceoff rating of +{awaySkaterMain.Card.Faceoff}.",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
+
+        GameplayController.Inst.GameData.PossPos.Clear();
 
         yield return null;
     }
@@ -68,13 +73,18 @@ public class FaceoffEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding FaceoffResult to the queue.");
 
-        // TODO
+        Skater homeSkaterMain = GetFaceoffCenter(true);
+        Skater awaySkaterMain = GetFaceoffCenter(false);
+
+        string faceoffWinner = DetermineFaceoff(homeSkaterMain, awaySkaterMain);
+        string faceoffPos = GetRandomPos();
+
+        Skater skaterPoss = GetSkaterPossession(faceoffPos, faceoffWinner == "Home");
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"After winning a faceoff, the puck is moved to a member of the winning center's team. They now control play to start this play segment.",
+            ActionText = $"",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -86,6 +96,68 @@ public class FaceoffEvents : MonoBehaviour {
     
 #endregion
 #region -------------------- Private Methods --------------------
-    
+    private Skater GetFaceoffCenter(bool isHome)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Getting the team faceoff center.");
+
+        GameTeam team = (isHome) ? GameplayController.Inst.GameData.HomeTeam : GameplayController.Inst.GameData.AwayTeam;
+
+        int currentLine = team.CurrentLine;
+        string currentCenter = $"C{currentLine.ToString()}";
+
+        return team.SkaterLineup[currentCenter];
+    }
+
+    private string DetermineFaceoff(Skater homeCenter, Skater awayCenter)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Determining the faceoff winner.");
+
+        int homeFaceoff = homeCenter.Card.Faceoff;
+        int awayFaceoff = awayCenter.Card.Faceoff;
+        int faceoffMid = 3;
+        int faceoffIndex = Random.Range(0,7);
+        int faceoffDiff = homeFaceoff - awayFaceoff;
+        int total = faceoffMid + faceoffDiff;
+
+        if (faceoffIndex > total) { return "Away"; }
+        else { return "Home"; }
+    }
+
+    private string GetRandomPos()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Getting a random position.");
+
+        int index = Random.Range(0,4);
+
+        switch (index)
+        {
+            case 0: return "LW";
+            case 1: return "RW";
+            case 2: return "LD";
+            case 3:
+            default: return "RD";
+        }
+    }
+
+    private Skater GetSkaterPossession(string pos, bool isHome)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Getting the skater possession.");
+
+        GameTeam team = isHome ? GameplayController.Inst.GameData.HomeTeam : GameplayController.Inst.GameData.AwayTeam;
+        GameTeam losingTeam = isHome ? GameplayController.Inst.GameData.AwayTeam : GameplayController.Inst.GameData.HomeTeam;
+
+        int currentLine = team.CurrentLine;
+        int currentPair = team.CurrentPair;
+
+        string currentSkater = (pos.Contains("D")) ? $"{pos}{currentPair.ToString()}" : $"{pos}{currentLine.ToString()}";
+
+        GameplayController.Inst.GameData.PossPos.Add($"C{currentLine.ToString()}");
+        GameplayController.Inst.GameData.PossPos.Add(currentSkater);
+        GameplayController.Inst.GameData.PossTeam = isHome ? "Home" : "Away";
+        team.SkaterLineup[$"C{currentLine.ToString()}"].Game.FaceoffsWon += 1;
+        losingTeam.SkaterLineup[$"C{currentLine.ToString()}"].Game.FaceoffsLost += 1;
+
+        return team.SkaterLineup[currentSkater];
+    }
 #endregion
 }}
