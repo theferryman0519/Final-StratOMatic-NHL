cf47ee437f07de736deb49f42b5c4b56a3164dd8
The Ferryman <ferrymanstudios@gmail.com>
2026-03-31 13:26:06 -0500

    Create EventRun.cs

---
+++ Scripts/Models/Events/EventRun.cs
@@ -0,0 +1,24 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class EventRun {
+    
+#region -------------------- Public Variables --------------------
+    public string InfoText { get; set; }
+    public string ActionText { get; set; }
+    public string PossPos { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
