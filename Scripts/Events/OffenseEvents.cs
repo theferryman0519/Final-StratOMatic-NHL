497059c52ea4297cb60dac095af4800c77912b9a
The Ferryman <ferrymanstudios@gmail.com>
2026-04-01 10:14:58 -0500

    Update OffenseEvents.cs

--- Scripts/Events/OffenseEvents.cs
+++ Scripts/Events/OffenseEvents.cs
@@ -19,6 +19,8 @@ public class OffenseEvents : MonoBehaviour {
 #endregion
 #region -------------------- Public Variables --------------------
     public ConstantController.ShotType SelectedShotType;
+
+    public Skater ShootingSkater;
 #endregion
 #region -------------------- Private Variables --------------------
     
@@ -31,13 +33,12 @@ public class OffenseEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ActionCard to the queue.");
 
-        // TODO
+        Skater possSkater = GameplayController.Inst.GetPossSkater();
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"At the start of each play, players must draw an Action Card. This will put the play into motion with a specific puck action.",
+            ActionText = $"{possSkater.Info.LastName} has the puck and is getting ready to draw an Action Card to get the play started.",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -49,13 +50,12 @@ public class OffenseEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding OutsideOptions to the queue.");
 
-        // TODO
+        Skater possSkater = GameplayController.Inst.GetPossSkater();
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"If a skater has an Outside Shot, they might have options to choose from instead of taking a shot. They could attempt a pass or drive the defense.",
+            ActionText = $"{possSkater.Info.FirstName} {possSkater.Info.LastName} is thinking about taking a shot, passing the puck, or driving the defense.",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -67,13 +67,59 @@ public class OffenseEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotStart to the queue.");
 
+        ShootingSkater = null;
+
+        Skater possSkater = GameplayController.Inst.GetPossSkater();
+        string shotType = string.Empty;
+
+        if (SelectedShotType == ConstantController.ShotType.Outside) { shotType = "shot from the perimeter"; }
+        else if (SelectedShotType == ConstantController.ShotType.Inside) { shotType = "shot from within the faceoff circles"; }
+        else { shotType = "shot from in tight near the crease"; }
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"A skater has three different types of shots: Outside, Inside, and Rebound/Breakaway. The closer to the net, the better the shots become.",
+            ActionText = $"{possSkater.Info.LastName} is looking to attempt a {shotType}.",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        ShootingSkater = possSkater;
+
+        yield return null;
+    }
+
+    public IEnumerator ShotResultLose()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotResultLose to the queue.");
+
+        Skater possSkater = GameplayController.Inst.GetPossSkater();
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"After taking a shot, one action that might occur is the shooter losing the puck to an opponent after blocking a shot.",
+            ActionText = $"It looks like {possSkater.Info.FirstName} {possSkater.Info.LastName} blocked the shot by {ShootingSkater.Info.LastName}.",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        GameplayController.Inst.StatsSet.AddBlockedShot(GameplayController.Inst.GameData.PossTeam == "Home", 1);
+
+        ShootingSkater = null;
+
+        yield return null;
+    }
+
+    public IEnumerator ShotResultSave()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotResultSave to the queue.");
+
         // TODO
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"",
+            ActionText = $"",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -81,17 +127,50 @@ public class OffenseEvents : MonoBehaviour {
         yield return null;
     }
 
-    public IEnumerator ShotResult()
+    public IEnumerator ShotResultRebound()
     {
-        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotResult to the queue.");
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotResultRebound to the queue.");
 
         // TODO
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"",
+            ActionText = $"",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        yield return null;
+    }
+
+    public IEnumerator ShotResultGoalieRating()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotResultGoalieRating to the queue.");
+
+        // TODO
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"",
+            ActionText = $"",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        yield return null;
+    }
+
+    public IEnumerator ShotResultGoal()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding ShotResultGoal to the queue.");
+
+        // TODO
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"",
+            ActionText = $"",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -107,9 +186,8 @@ public class OffenseEvents : MonoBehaviour {
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"",
+            ActionText = $"",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -125,9 +203,8 @@ public class OffenseEvents : MonoBehaviour {
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"",
+            ActionText = $"",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
@@ -135,17 +212,67 @@ public class OffenseEvents : MonoBehaviour {
         yield return null;
     }
 
-    public IEnumerator PassingResult()
+    public IEnumerator PassingResultLose()
     {
-        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding PassingResult to the queue.");
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding PassingResultLose to the queue.");
 
         // TODO
 
         EventRun newEventRun = new EventRun
         {
-            InfoText = "",
-            ActionText = "",
-            PossPos = "",
+            InfoText = $"",
+            ActionText = $"",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        yield return null;
+    }
+
+    public IEnumerator PassingResultLoseShot()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding PassingResultLoseShot to the queue.");
+
+        // TODO
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"",
+            ActionText = $"",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        yield return null;
+    }
+
+    public IEnumerator PassingResultShot()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding PassingResultShot to the queue.");
+
+        // TODO
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"",
+            ActionText = $"",
+        };
+
+        EventsController.Inst.CurrentEventRun = newEventRun;
+
+        yield return null;
+    }
+
+    public IEnumerator PassingResultOptions()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding PassingResultOptions to the queue.");
+
+        // TODO
+
+        EventRun newEventRun = new EventRun
+        {
+            InfoText = $"",
+            ActionText = $"",
         };
 
         EventsController.Inst.CurrentEventRun = newEventRun;
