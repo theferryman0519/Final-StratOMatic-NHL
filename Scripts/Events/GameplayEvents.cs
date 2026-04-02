6895f048c07f0ea46ba25353db5cb73369cea82c
The Ferryman <ferrymanstudios@gmail.com>
2026-04-02 08:54:16 -0500

    Update GameplayEvents.cs

--- Scripts/Events/GameplayEvents.cs
+++ Scripts/Events/GameplayEvents.cs
@@ -127,15 +127,14 @@ public class GameplayEvents : MonoBehaviour {
         {
             case 0: RunEvent(DefenseEvents.IntimidationStart); break;
             case 1: RunEvent(DefenseEvents.IntimidationResultSteal); break;
-            case 2: RunEvent(DefenseEvents.IntimidationResultStealShot); break;
-            case 3: RunEvent(DefenseEvents.IntimidationResultShot); break;
-            case 4: RunEvent(DefenseEvents.DefendingStart); break;
-            case 5: RunEvent(DefenseEvents.DefendingResultSteal); break;
-            case 6: RunEvent(DefenseEvents.DefendingResultStealShot); break;
-            case 7: RunEvent(DefenseEvents.DefendingResultStealShotIntimidation); break;
-            case 8: RunEvent(DefenseEvents.DefendingResultShot); break;
-            case 9: RunEvent(DefenseEvents.DefendingResultShotIntimidation); break;
-            case 10: RunEvent(DefenseEvents.DefendingResultPenalty); break;
+            case 2: RunEvent(DefenseEvents.IntimidationResultShot); break;
+            case 3: RunEvent(DefenseEvents.DefendingStart); break;
+            case 4: RunEvent(DefenseEvents.DefendingResultSteal); break;
+            case 5: RunEvent(DefenseEvents.DefendingResultStealShot); break;
+            case 6: RunEvent(DefenseEvents.DefendingResultStealShotIntimidation); break;
+            case 7: RunEvent(DefenseEvents.DefendingResultShot); break;
+            case 8: RunEvent(DefenseEvents.DefendingResultShotIntimidation); break;
+            case 9: RunEvent(DefenseEvents.DefendingResultPenalty); break;
             default: break;
         }
     }
