8fc1e5d0be7ff34456bc677f73e1d5ed3bcc9a82
The Ferryman <ferrymanstudios@gmail.com>
2026-04-03 15:32:13 -0500

    Update GameplayStatsSet.cs

--- Scripts/Gameplay/GameplayStatsSet.cs
+++ Scripts/Gameplay/GameplayStatsSet.cs
@@ -41,36 +41,129 @@ public class GameplayStatsSet : MonoBehaviour {
     
 #endregion
 #region -------------------- Skater Stats Methods --------------------
-    public void AddFaceoffWon(bool isHome, int delta)
+    public void AddGoal(Skater skater, ConstantController.GoalType goalType, int delta)
     {
-        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to faceoff won.");
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to goals and points.");
+        skater.Game.Goals += delta;
+        skater.Game.Points += delta;
 
-        GameTeam team = isHome ? GameplayController.Inst.GameData.HomeTeam : GameplayController.Inst.GameData.AwayTeam;
-        int currentLine = team.CurrentLine;
-        string center = $"C{currentLine}";
+        if (goalType == ConstantController.GoalType.Powerplay)
+        {
+            skater.Game.PowerplayGoals += delta;
+            skater.Game.PowerplayPoints += delta;
+        }
 
-        team.SkaterLineup[center].Game.FaceoffsWon += delta;
+        else if (goalType == ConstantController.GoalType.Shorthanded)
+        {
+            skater.Game.ShorthandedGoals += delta;
+            skater.Game.ShorthandedPoints += delta;
+        }
     }
 
-    public void AddFaceoffLost(bool isHome, int delta)
+    public void AddAssist(Skater skater, ConstantController.GoalType goalType, int delta)
     {
-        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to faceoff won.");
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to assists and points.");
+        skater.Game.Assists += delta;
+        skater.Game.Points += delta;
 
-        GameTeam team = isHome ? GameplayController.Inst.GameData.HomeTeam : GameplayController.Inst.GameData.AwayTeam;
-        int currentLine = team.CurrentLine;
-        string center = $"C{currentLine}";
+        if (goalType == ConstantController.GoalType.Powerplay)
+        {
+            skater.Game.PowerplayAssists += delta;
+            skater.Game.PowerplayPoints += delta;
+        }
 
-        team.SkaterLineup[center].Game.FaceoffsLost += delta;
+        else if (goalType == ConstantController.GoalType.Shorthanded)
+        {
+            skater.Game.ShorthandedAssists += delta;
+            skater.Game.ShorthandedPoints += delta;
+        }
     }
 
-    public void AddBlockedShot(bool isHome, int delta)
+    public void AddPlusMinus(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to plus/minus.");
+        skater.Game.PlusMinus += delta;
+    }
+
+    public void AddPenaltyMinute(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to penalty minutes.");
+        skater.Game.PenaltyMinutes += delta;
+    }
+
+    public void AddShot(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to shots.");
+        skater.Game.Shots += delta;
+    }
+
+    public void AddGiveaway(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to giveaways.");
+        skater.Game.Giveaways += delta;
+    }
+
+    public void AddTakeaway(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to takeaways.");
+        skater.Game.Takeaways += delta;
+    }
+
+    public void AddHit(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to hits.");
+        skater.Game.Hits += delta;
+    }
+
+    public void AddBlockedShot(Skater skater, int delta)
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to blocked shot.");
+        skater.Game.BlockedShots += delta;
+    }
+    
+    public void AddFaceoffWon(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to faceoff won.");
+        skater.Game.FaceoffsWon += delta;
+    }
 
-        GameTeam team = isHome ? GameplayController.Inst.GameData.HomeTeam : GameplayController.Inst.GameData.AwayTeam;
-        string possPos = GameplayController.Inst.GameData.PossPos[GameplayController.Inst.GameData.PossPos.Count - 1];
+    public void AddFaceoffLost(Skater skater, int delta)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding to faceoff won.");
+        skater.Game.FaceoffsLost += delta;
+    }
 
-        team.SkaterLineup[possPos].Game.BlockedShots += delta;
+    public void AddShiftSegment(Skater skater)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Adding seconds played.");
+        skater.Game.SecondsPlayed += 40;
+    }
+
+    public void LowerStamina(Skater skater)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Lowering stamina.");
+
+        string fatigueRating = skater.Card.Fatigue;
+
+        switch (fatigueRating)
+        {
+            case "AA": skater.Game.Stamina -= 25; break;
+            case "A": skater.Game.Stamina -= 20; break;
+            case "B": skater.Game.Stamina -= 15; break;
+            case "C": skater.Game.Stamina -= 10; break;
+            case "D": skater.Game.Stamina -= 5; break;
+        }
+
+        if (skater.Game.Stamina < 0)
+        {
+            skater.Game.Stamina = 0;
+        }
+    }
+
+    public void ResetStamina(Skater skater)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Resetting stamina.");
+        skater.Game.Stamina = 100;
     }
 #endregion
 #region -------------------- Goalie Stats Methods --------------------
