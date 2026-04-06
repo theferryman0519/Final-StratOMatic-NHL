736bd73d01b78abe5249a0136d8abd5deeed02df
The Ferryman <ferrymanstudios@gmail.com>
2026-04-06 13:27:14 -0500

    Update GoalEvents.cs

--- Scripts/Events/GoalEvents.cs
+++ Scripts/Events/GoalEvents.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 // Game Dependencies
 using SoM.Controllers;
@@ -37,6 +38,10 @@ public class GoalEvents : MonoBehaviour {
     public IEnumerator GoalieRatingStart()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding GoalieRatingStart to the queue.");
+
+        GameTeam defTeam = GameplayController.Inst.GameData.PossTeam == "Home" ? GameplayController.Inst.GameData.AwayTeam : GameplayController.Inst.GameData.HomeTeam;
+        DefendingGoalie = defTeam.GoalieLineup["G"];
+        ShootingSkater = EventsController.Inst.OffenseEvents.ShootingSkater;
 
         EventRun newEventRun = new EventRun
         {
@@ -118,6 +123,8 @@ public class GoalEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding GoalCheck to the queue.");
 
+        ShootingSkater = EventsController.Inst.OffenseEvents.ShootingSkater;
+
         EventRun newEventRun = new EventRun
         {
             InfoText = $"Some goal actions for skaters have a goal threshold. The higher the number, the more likely a goal will occur.",
@@ -150,7 +157,9 @@ public class GoalEvents : MonoBehaviour {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Adding Goal to the queue.");
 
-        string announcement = DetermineGoalAnnouncment();
+        ShootingSkater = EventsController.Inst.OffenseEvents.ShootingSkater;
+
+        string announcement = DetermineGoalAnnouncement();
 
         EventRun newEventRun = new EventRun
         {
@@ -168,87 +177,260 @@ public class GoalEvents : MonoBehaviour {
     public void DetermineGoalieRatingOutcome()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Determining the goalie rating outcome.");
+        
+        int randomNumber = Random.Range(2,13);
+        string ratingAction = DefendingGoalie.Card.GoalieRatingActions[randomNumber - 1];
 
-        // TODO
-        // Determine goalie rating actions of defending goalie
-        // Determine random 2d6 result
-        // Determine goalie rating action based on dice sum
-        // If result is a save:
-            // Set possession team to none
-            // Reset possession position tracker
-            // Set continue action to goalie rating result save
-        // If result is a breakaway:
-            // Determine random skater position
-            // Add to possession position tracker
-            // Set shot type as RebBreak
-            // Set continue action to goalie rating result breakaway
-        // If result is a penalty:
-            // Set penalty skater as center from fourth line
-            // Add penalty minutes to defending goalie
-            // Set continue action to goalie rating result penalty
-        // If result is a goal:
-            // Set continue action to goalie rating result goal
+        if (ratingAction == "SAVE")
+        {
+            GameplayController.Inst.StatsSet.ClearPossPos();
+            GameplayController.Inst.StatsSet.SetPossTeam("None");
+
+            EventsController.Inst.RunGoalEvent(1);
+        }
+
+        else if (ratingAction == "BREAKAWAY")
+        {
+            string newPos = GetRandomPos();
+
+            GameTeam defTeam = GameplayController.Inst.GameData.PossTeam == "Home" ? GameplayController.Inst.GameData.AwayTeam : GameplayController.Inst.GameData.HomeTeam;
+            string newTeamPos = GameplayController.Inst.GameData.PossTeam == "Home" ? "Away" : "Home";
+
+            int lineNum = defTeam.CurrentLine;
+            int pairNum = defTeam.CurrentPair;
+
+            string newSkaterPos = newPos.Contains("D") ? $"{newPos}{pairNum}" : $"{newPos}{lineNum}";
+            Skater newPossSkater = defTeam.SkaterLineup[newSkaterPos];
+
+            GameplayController.Inst.StatsSet.ClearPossPos();
+            GameplayController.Inst.StatsSet.SetPossTeam(newTeamPos);
+            GameplayController.Inst.StatsSet.AddPossPos("G");
+            GameplayController.Inst.StatsSet.AddPossPos(newSkaterPos);
+
+            EventsController.Inst.GameplayEvents.OffenseEvents.SelectedShotType = ConstantController.ShotType.RebBreak;
+            EventsController.Inst.GameplayEvents.OffenseEvents.ShootingSkater = newPossSkater;
+
+            EventsController.Inst.RunGoalEvent(2);
+        }
+
+        else if (ratingAction == "PENALTY")
+        {
+            GameTeam defTeam = GameplayController.Inst.GameData.PossTeam == "Home" ? GameplayController.Inst.GameData.AwayTeam : GameplayController.Inst.GameData.HomeTeam;
+            Skater inBoxSkater = defTeam.SkaterLineup["LW4"];
+
+            GameplayController.Inst.StatsSet.ClearPossPos();
+            GameplayController.Inst.StatsSet.SetPossTeam("None");
+
+            EventsController.Inst.GameplayEvents.PenaltyEvents.PenaltySkater = inBoxSkater;
+            EventsController.Inst.GameplayEvents.PenaltyEvents.PenaltyGoalie = DefendingGoalie;
+
+            EventsController.Inst.RunGoalEvent(3);
+        }
+
+        else
+        {
+            GoalThreshold = 20;
+
+            EventsController.Inst.RunGoalEvent(4);
+        }
     }
 
     public void DetermineGoalOutcome()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Determining the goal outcome.");
+        
+        int randomNumber = Random.Range(1,21);
+        bool isUnderThreshold = randomNumber <= GoalThreshold;
 
-        // TODO
-        // Determine goal threshold number
-        // Determine random number between 1 and 20 (inclusive)
-        // If number is less than or equal to threshold number:
-            // Set continue action to goal
-        // If number is greater than threshold number:
-            // Set possession team to none
-            // Reset possession position tracker
-            // Set continue action to no goal
+        if (isUnderThreshold)
+        {
+            EventsController.Inst.RunGoalEvent(7);
+        }
+
+        else
+        {
+            GameplayController.Inst.StatsSet.ClearPossPos();
+            GameplayController.Inst.StatsSet.SetPossTeam("None");
+
+            EventsController.Inst.RunGoalEvent(6);
+        }
     }
 
     public void DetermineAfterGoalEvent()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Determining the event after a goal is scored.");
+        
+        int currentPeriod = GameplayController.Inst.GameData.Period;
 
-        // TODO
-        // Determine current period
-        // If period is 4 or higher:
-            // Set continue action to end of game
-        // If period is 1, 2, or 3:
-            // Set continue action ot puck drop
+        GameplayController.Inst.StatsSet.ClearPossPos();
+        GameplayController.Inst.StatsSet.SetPossTeam("None");
+
+        if (currentPeriod > 3)
+        {
+            EventsController.Inst.RunGameFlowEvent(5);
+        }
+
+        else
+        {
+            EventsController.Inst.RunFaceoffEvent(0);
+        }
     }
 
-    public string DetermineGoalAnnouncment()
+    public string DetermineGoalAnnouncement()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Determining the goal announcement.");
+        
+        GameplayController.Inst.StatsSet.AddGoal(ShootingSkater, EventsController.Inst.OffenseEvents.SelectedShotType, 1);
 
-        // TODO
-        // Add goal to shooting skater
-        // Add goal against for defending goalie
-        // Determine up to two prior skaters in possession position tracker
-        // Add assists to prior skaters
-        // Determine goal type
-        // If goal is shorthanded:
-            // Add shorthanded goal to shooting skater
-            // Add shorthanded assists to prior skaters
-        // If goal is powerplay:
-            // Add powerplay goal to shooting skater
-            // Add powerplay assists to prior skaters
-        // Else:
-            // Add pluses to all on ice for scoring team
-            // Add minuses to all on ice for defending team
+        bool isHome = GameplayController.Inst.GameData.PossTeam == "Home";
+        GameTeam possTeam = isHome ? GameplayController.Inst.GameData.HomeTeam : GameplayController.Inst.GameData.AwayTeam;
+
+        Skater assistSkaterA = null;
+        Skater assistSkaterB = null;
+
+        Goalie assistGoalieA = null;
+        Goalie assistGoalieB = null;
+
+        List<string> assistPos = new();
+
+        foreach (string pos in GameplayController.Inst.GameData.PossPos)
+        {
+            if (!assistPos.Contains(pos)) { assistPos.Add(pos); }
+        }
+
+        if (assistPos.Count > 2)
+        {
+            string assistPosA = assistPos[assistPos.Count - 2];
+            string assistPosB = assistPos[assistPos.Count - 3];
+
+            if (assistPosA == "G")
+            {
+                assistGoalieA = possTeam.GoalieLineup["G"];
+                GameplayController.Inst.StatsSet.AddGoalieAssist(assistGoalieA, 1);
+            }
+
+            else
+            {
+                assistSkaterA = possTeam.SkaterLineup[assistPosA];
+                GameplayController.Inst.StatsSet.AddAssist(assistSkaterA, EventsController.Inst.OffenseEvents.SelectedShotType, 1);
+            }
+
+            if (assistPosB == "G")
+            {
+                assistGoalieB = possTeam.GoalieLineup["G"];
+                GameplayController.Inst.StatsSet.AddGoalieAssist(assistGoalieB, 1);
+            }
+
+            else
+            {
+                assistSkaterB = possTeam.SkaterLineup[assistPosB];
+                GameplayController.Inst.StatsSet.AddAssist(assistSkaterB, EventsController.Inst.OffenseEvents.SelectedShotType, 1);
+            }
+        }
+
+        else if (assistPos.Count == 2)
+        {
+            string assistPosA = assistPos[0];
+
+            if (assistPosA == "G") { assistGoalieA = possTeam.GoalieLineup["G"]; }
+            else { assistSkaterA = possTeam.SkaterLineup[assistPosA]; }
+        }
+
+        if (EventsController.Inst.OffenseEvents.SelectedShotType != ConstantController.ShotType.Powerplay && 
+            EventsController.Inst.OffenseEvents.SelectedShotType != ConstantController.ShotType.Shorthanded
+        )
+        {
+            GameplayController.Inst.StatsSet.AddTeamPlusMinus(isHome, 1);
+            GameplayController.Inst.StatsSet.AddTeamPlusMinus(!isHome, -1);
+        }
         
         string goalAnnouncement = string.Empty;
+        string goalTypeString = string.Empty;
 
-        // TODO
-        // Determine game type for specific goal announcement
-        // Determine goal number for specific goal annoucement
-        // Add string for goal scorer
-        // Add string for assists
+        if (EventsController.Inst.OffenseEvents.SelectedShotType == ConstantController.ShotType.Powerplay) { goalTypeString = "on the powerplay"; }
+        else if (EventsController.Inst.OffenseEvents.SelectedShotType == ConstantController.ShotType.Shorthanded) { goalTypeString = "while shorthanded"; }
+
+        string goalCountString = "With their first goal of the night";
+
+        if (ShootingSkater.Game.Goals == 2) { goalCountString = "Netting their second goal tonight"; }
+        else if (ShootingSkater.Game.Goals == 3) { goalCountString = "With that hat trick goal"; }
+        else if (ShootingSkater.Game.Goals > 3) { goalCountString = "After many goals scored tonight"; }
+
+        string gameTypeString = string.Empty;
+
+        if (GameplayController.Inst.GameData.Type == "Season")
+        {
+            int seasonGoals = ShootingSkater.Season.Goals += ShootingSkater.Game.Goals;
+            gameTypeString = $" (number {seasonGoals} on the season)";
+        }
+
+        else if (GameplayController.Inst.GameData.Type == "Playoff")
+        {
+            int playoffGoals = ShootingSkater.Playoff.Goals += ShootingSkater.Game.Goals;
+            gameTypeString = $" (number {playoffGoals} of the playoffs)";
+        }
+
+        goalAnnouncement = $"{goalCountString}, {ShootingSkater.Info.FirstName} {ShootingSkater.Info.LastName} scores {goalTypeString}{gameTypeString}.";
+
+        string addedAssistsString = string.Empty;
+
+        if (assistSkaterA == null)
+        {
+            if (assistGoalieA != null)
+            {
+                addedAssistsString = $" The goal is assisted by {assistGoalieA.Info.FirstName} {assistsGoalieA.Info.LastName}";
+            }
+
+            else
+            {
+                addedAssistsString = " The goal was unassisted";
+            }
+        }
+
+        else
+        {
+            addedAssistsString = $" The goal is assisted by {assistSkaterA.Info.FirstName} {assistSkaterA.Info.LastName}";
+        }
+
+        if (assistSkaterB == null)
+        {
+            if (assistGoalieB != null)
+            {
+                addedAssistsString += $" and by {assistGoalieB.Info.FirstName} {assistGoalieB.Info.LastName}.";
+            }
+
+            else
+            {
+                addedAssistsString += ".";
+            }
+        }
+
+        else
+        {
+            addedAssistsString += $" and by {assistSkaterB.Info.FirstName} {assistSkaterB.Info.LastName}.";
+        }
+
+        goalAnnouncement += addedAssistsString;
 
         return goalAnnouncement;
     }
 #endregion
 #region -------------------- Private Methods --------------------
-    
+    private string GetRandomPos()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Getting a random position.");
+
+        int index = Random.Range(0,5);
+
+        switch (index)
+        {
+            case 0: return "C";
+            case 1: return "LW";
+            case 2: return "RW";
+            case 3: return "LD";
+            case 4:
+            default: return "RD";
+        }
+    }
 #endregion
 }}
