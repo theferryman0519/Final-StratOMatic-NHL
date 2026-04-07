93640044d00803d46d42f4af97b979f941ecec18
The Ferryman <ferrymanstudios@gmail.com>
2026-04-07 13:21:22 -0500

    Update SkaterPlayoff.cs

--- Scripts/Models/Skaters/SkaterPlayoff.cs
+++ Scripts/Models/Skaters/SkaterPlayoff.cs
@@ -16,7 +16,7 @@ namespace SoM.Models {
 public class SkaterPlayoff {
     
 #region -------------------- Public Variables --------------------
-    public string Id { get; set; }
+    public string UserId { get; set; }
 
     public int GamesPlayed { get; set; }
     public int Goals { get; set; }
