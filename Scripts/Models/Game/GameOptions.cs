499cf7863dec19cc14ded40697d750e32ac1ca5c
The Ferryman <ferrymanstudios@gmail.com>
2026-04-07 13:46:03 -0500

    Create GameOptions.cs

---
+++ Scripts/Models/Game/GameOptions.cs
@@ -0,0 +1,27 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class GameOptions {
+    
+#region -------------------- Public Variables --------------------
+    public bool LineChangesOn { get; set; }
+    public bool FatigueOn { get; set; }
+    public bool InjuriesOn { get; set; }
+
+    public int AiDifficulty { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
