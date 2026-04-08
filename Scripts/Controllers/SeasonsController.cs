8e614fe8110c1ae78c4c0500e1a480201c3c9ec2
The Ferryman <ferrymanstudios@gmail.com>
2026-04-08 09:21:30 -0500

    Update SeasonsController.cs

--- Scripts/Controllers/SeasonsController.cs
+++ Scripts/Controllers/SeasonsController.cs
@@ -42,18 +42,30 @@ public class SeasonsController : Singleton<SeasonsController> {
         LoadSeasonData();
     }
 
-    public void CreateNewSeason()
+    public void CreateNewSeason(Action continueAction = null)
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Creating a new season.");
 
-        // TODO
+        SeasonData = null;
+
+        SeasonData = new Season
+        {
+            Id = Guid.NewGuid().ToString(),
+            League = string.Empty,
+            Version = -1,
+            GameNights = new(),
+        };
+
+        continueAction?.Invoke();
     }
 
-    public void LoadCurrentSeason()
+    public void LoadCurrentSeason(Action continueAction = null)
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Loading the current season.");
 
-        // TODO
+        // TODO: Load GameNights from teams
+
+        continueAction?.Invoke();
     }
 #endregion
 #region -------------------- Private Methods --------------------
@@ -72,6 +84,7 @@ public class SeasonsController : Singleton<SeasonsController> {
                     Id = seasonData.Id,
                     League = seasonData.League,
                     Version = seasonData.Version,
+                    GameNights = new(),
                 };
 
                 ConstantController.LeagueType leagueType = ConstantController.LeagueType.None;
@@ -97,12 +110,99 @@ public class SeasonsController : Singleton<SeasonsController> {
                 };
 
                 SeasonData.Team = userGameTeam;
+                SeasonData.Team.SkaterLineup = await SetSkaterLineup(seasonData.SkaterLineup);
+                SeasonData.Team.GoalieLineup = await SetGoalieLineup(seasonData.GoalieLineup);
 
                 SeasonGameNight = seasonData.GameNight;
             }
 
             CoreController.Inst.LoadingStepCompleted();
         });
+    }
+
+    private async Task<Dictionary<string, Skater>> SetSkaterLineup(List<string> skaterIds)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Setting the skater lineup.");
+
+        Dictionary<string, Skater> skaterLineup = new();
+
+        if (skaterIds.Count < 18)
+        {
+            return skaterLineup;
+        }
+
+        List<string> posStrings = new() { "C1", "LW1", "RW1", "C2", "LW2", "RW2", "C3", "LW3", "RW3", "C4", "LW4", "RW4", "LD1", "RD1", "LD2", "RD2", "LD3", "RD3" };
+
+        for (int i = 0; i < posStrings.Count; i++)
+        {
+            Skater nextSkater = GetSkaterById(skaterIds[i]);
+
+            if (nextSkater != null) { skaterLineup.Add(posStrings[i], nextSkater); }
+        }
+
+        return skaterLineup;
+    }
+
+    private async Task<Dictionary<string, Goalie>> SetGoalieLineup(List<string> goalieIds)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Setting the goalie lineup.");
+
+        Dictionary<string, Goalie> goalieLineup = new();
+
+        if (goalieIds.Count < 1)
+        {
+            return goalieLineup;
+        }
+
+        Goalie startingGoalie = GetGoalieById(goalieIds[0]);
+
+        if (startingGoalie != null) { goalieLineup.Add("G", startingGoalie); }
+
+        return goalieLineup;
+    }
+
+    private Skater GetSkaterById(string id)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Getting the skater by id.");
+
+        Skater resultSkater = new Skater { };
+
+        List<Skater> teamSkaters = new();
+
+        if (SeasonData.League == "NHL") { teamSkaters = SkatersController.Inst.NhlSkaters[SeasonData.Team.Code]; }
+        else { teamSkaters = SkatersController.Inst.PwhlSkaters[SeasonData.Team.Code]; }
+
+        foreach (Skater skater in teamSkaters)
+        {
+            if (id == skater.Id)
+            {
+                resultSkater = skater;
+            }
+        }
+
+        return resultSkater;
+    }
+
+    private Goalie GetGoalieById(string id)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Getting the goalie by id.");
+
+        Goalie resultGoalie = new Goalie { };
+
+        List<Goalie> teamGoalies = new();
+
+        if (SeasonData.League == "NHL") { teamGoalies = GoaliesController.Inst.NhlGoalies[SeasonData.Team.Code]; }
+        else { teamGoalies = GoaliesController.Inst.PwhlGoalies[SeasonData.Team.Code]; }
+
+        foreach (Goalie goalie in teamGoalies)
+        {
+            if (id == goalie.Id)
+            {
+                resultGoalie = goalie;
+            }
+        }
+
+        return resultGoalie;
     }
 #endregion
 }}
