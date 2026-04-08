d9bab649af1e2c5a015de29eb47a5d3d1626c298
The Ferryman <ferrymanstudios@gmail.com>
2026-04-08 10:00:19 -0500

    Update GoalieSeason.cs

--- Scripts/Models/Goalies/GoalieSeason.cs
+++ Scripts/Models/Goalies/GoalieSeason.cs
@@ -26,6 +26,7 @@ public class GoalieSeason {
     public int ShotsAgainst { get; set; }
     public int Assists { get; set; }
     public int PenaltyMinutes { get; set; }
+    public int Stamina { get; set; }
 #endregion
 #region -------------------- Private Variables --------------------
     
