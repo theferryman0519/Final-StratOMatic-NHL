205796fffbf9c0cf5b0301eb31a053ece15027cd
The Ferryman <ferrymanstudios@gmail.com>
2026-04-08 09:25:47 -0500

    Update TeamSeason.cs

--- Scripts/Models/Teams/TeamSeason.cs
+++ Scripts/Models/Teams/TeamSeason.cs
@@ -15,9 +15,7 @@ namespace SoM.Models {
 [System.Serializable]
 public class TeamSeason {
     
-#region -------------------- Public Variables --------------------
-    public string Id { get; set; }
-    
+#region -------------------- Public Variables --------------------    
     public int GamesPlayed { get; set; }
     public int Wins { get; set; }
     public int Losses { get; set; }
