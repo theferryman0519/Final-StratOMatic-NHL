ee740ce539cc8052106fc97550e49ef9ff5b3a92
The Ferryman <ferrymanstudios@gmail.com>
2026-04-10 11:40:02 -0500

    Create EditLinesPanel.cs

---
+++ Scripts/Ui/Panels/Gameplay/EditLinesPanel.cs
@@ -0,0 +1,71 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Controllers;
+using SoM.Models;
+
+namespace SoM.Ui {
+public class EditLinesPanel : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Button Elements")]
+	[SerializeField] private SoM_Button _closeButton;
+
+    [Header("Main Elements")]
+	[SerializeField] private CanvasGroup _mainElement;
+	[SerializeField] private RectTransform _mainPanel;
+#endregion
+#region -------------------- Public Variables --------------------
+    public CanvasGroup CanvasGroup => _mainElement;
+    
+    public RectTransform MainPanel => _mainPanel;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    public void InitializeEditLinesPanel()
+	{
+        _mainElement.alpha = 0f;
+
+        _closeButton.SetListener(() => { ClosePanel(); });
+
+        AnimationController.Inst.FadeInPanel(_mainElement, _mainPanel, () =>
+        {
+            _mainElement.alpha = 1f;
+        });
+	}
+
+    public void ClosePanel(Action continueAction = null)
+	{
+		AnimationController.Inst.FadeOutPanel(_mainElement, _mainPanel, () =>
+		{
+			HidePanel();
+			continueAction?.Invoke();
+		});
+	}
+
+    public void HidePanel()
+    {
+        _mainElement.alpha = 0f;
+        this.gameObject.SetActive(false);
+    }
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
