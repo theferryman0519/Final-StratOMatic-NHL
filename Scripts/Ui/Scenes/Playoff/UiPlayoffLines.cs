95354fcecc5d40bf3481eb336c62b34e090b5983
The Ferryman <ferrymanstudios@gmail.com>
2026-04-13 13:58:34 -0500

    Update UiPlayoffLines.cs

--- Scripts/Ui/Scenes/Playoff/UiPlayoffLines.cs
+++ Scripts/Ui/Scenes/Playoff/UiPlayoffLines.cs
@@ -144,8 +144,17 @@ public class UiPlayoffLines : UiSceneBase {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the playoff game ready screen.");
 
-		// TODO: Set team edit lines
-        // TODO: Set lines into PlayerPrefs
+		GameplayController.Inst.GameData.HomeTeam.SkaterLineup.Clear();
+		GameplayController.Inst.GameData.HomeTeam.GoalieLineup.Clear();
+
+		foreach (KeyValuePair<string, EditLinePositionPrefab> position in positionObjectsDict)
+		{
+			if (!position.Key.Contains("G")) { GameplayController.Inst.GameData.HomeTeam.SkaterLineup.Add(position.Key, position.Value.ThisSkater); }
+			else { GameplayController.Inst.GameData.HomeTeam.GoalieLineup.Add("G", position.Value.ThisGoalie); }
+		}
+
+		PlayoffsController.Inst.PlayoffData.Team.SkaterLineup = GameplayController.Inst.GameData.HomeTeam.SkaterLineup;
+		PlayoffsController.Inst.PlayoffData.Team.GoalieLineup = GameplayController.Inst.GameData.HomeTeam.GoalieLineup;
 
 		GoToNewScene(CoreController.Inst.Scene_Playoff06);
     }
