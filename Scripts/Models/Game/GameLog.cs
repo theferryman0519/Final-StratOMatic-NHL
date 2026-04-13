861148f34bda915fa70808383cda50df5000822e
The Ferryman <ferrymanstudios@gmail.com>
2026-04-13 11:58:19 -0500

    Create GameLog.cs

---
+++ Scripts/Models/Game/GameLog.cs
@@ -0,0 +1,26 @@
+// Main Dependencies
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace SoM.Models {
+public class GameLog {
+    
+#region -------------------- Public Variables --------------------
+    public int Period { get; set; }
+
+    public string GameTime { get; set; }
+    public string Action { get; set; }
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+}}
