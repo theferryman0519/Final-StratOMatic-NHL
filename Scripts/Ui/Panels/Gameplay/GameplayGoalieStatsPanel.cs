dfe7f33d459f0df70992976a6030a3791dddc085
The Ferryman <ferrymanstudios@gmail.com>
2026-04-13 11:26:54 -0500

    Update GameplayGoalieStatsPanel.cs

--- Scripts/Ui/Panels/Gameplay/GameplayGoalieStatsPanel.cs
+++ Scripts/Ui/Panels/Gameplay/GameplayGoalieStatsPanel.cs
@@ -18,6 +18,17 @@ public class GameplayGoalieStatsPanel : MonoBehaviour {
 #region -------------------- Serialized Variables --------------------
     [Header("Button Elements")]
 	[SerializeField] private SoM_Button _closeButton;
+	[SerializeField] private SoM_Button _returnButton;
+
+	[Header("Dropdown Elements")]
+	[SerializeField] private SoM_Dropdown _statsDropdown;
+
+	[Header("Page Elements")]
+	[SerializeField] private GameObject _infoPage;
+	[SerializeField] private GameObject _cardPage;
+
+	[SerializeField] private List<TMP_Text> _infoTexts = new();
+	[SerializeField] private List<TMP_Text> _cardTexts = new();
 
     [Header("Main Elements")]
 	[SerializeField] private CanvasGroup _mainElement;
@@ -38,11 +49,20 @@ public class GameplayGoalieStatsPanel : MonoBehaviour {
     
 #endregion
 #region -------------------- Public Methods --------------------
-    public void InitializeMenuPanel()
+    public void InitializeGoalieStatsPanel(Goalie goalie)
 	{
+		if (goalie == null) { return; }
+
         _mainElement.alpha = 0f;
 
         _closeButton.SetListener(() => { ClosePanel(); });
+		_returnButton.SetListener(() => { ClosePanel(); });
+
+		_statsDropdown.SetListener(ChangeStatsOption);
+
+		ChangeStatsOption(0);
+
+		SetTexts(goalie);
 
         AnimationController.Inst.FadeInPanel(_mainElement, _mainPanel, () =>
         {
@@ -62,6 +82,44 @@ public class GameplayGoalieStatsPanel : MonoBehaviour {
 	}
 #endregion
 #region -------------------- Private Methods --------------------
-    
+    private void ChangeStatsOption(int option)
+    {
+		switch (option)
+		{
+			case 1:
+				_infoPage.SetActive(false);
+				_cardPage.SetActive(true);
+				break;
+			case 0:
+			default:
+				_infoPage.SetActive(true);
+				_cardPage.SetActive(false);
+				break;
+		}
+    }
+
+	private void SetTexts(Goalie goalie)
+	{
+		_infoTexts[0].text = $"Penalty: {goalie.Card.Penalty}";
+		_infoTexts[1].text = $"Fatigue: {goalie.Card.Fatigue}";
+		
+		_infoTexts[2].text = $"Goals Against: {goalie.Game.GoalsAgainst}" + "\n" +
+			$"Shots Against: {goalie.Game.ShotsAgainst}";
+		
+		_infoTexts[3].text = $"Assists: {goalie.Game.Assists}" + "\n" +
+			$"PIM: {goalie.Game.PenaltyMinutes}";
+		
+		_cardTexts[0].text = $"02) {goalie.Card.GoalieRatingActions[0]}" + "\n" +
+			$"03) {goalie.Card.GoalieRatingActions[1]}" + "\n" +
+			$"04) {goalie.Card.GoalieRatingActions[2]}" + "\n" +
+			$"05) {goalie.Card.GoalieRatingActions[3]}" + "\n" +
+			$"06) {goalie.Card.GoalieRatingActions[4]}" + "\n" +
+			$"07) {goalie.Card.GoalieRatingActions[5]}" + "\n" +
+			$"08) {goalie.Card.GoalieRatingActions[6]}" + "\n" +
+			$"09) {goalie.Card.GoalieRatingActions[7]}" + "\n" +
+			$"10) {goalie.Card.GoalieRatingActions[8]}" + "\n" +
+			$"11) {goalie.Card.GoalieRatingActions[9]}" + "\n" +
+			$"12) {goalie.Card.GoalieRatingActions[10]}";
+	}
 #endregion
 }}
