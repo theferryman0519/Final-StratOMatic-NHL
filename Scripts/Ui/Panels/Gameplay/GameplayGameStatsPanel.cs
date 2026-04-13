decb2df67a64ad1011ee6e29a2e2ecf6fe1fff35
The Ferryman <ferrymanstudios@gmail.com>
2026-04-13 11:39:24 -0500

    Update GameplayGameStatsPanel.cs

--- Scripts/Ui/Panels/Gameplay/GameplayGameStatsPanel.cs
+++ Scripts/Ui/Panels/Gameplay/GameplayGameStatsPanel.cs
@@ -18,6 +18,13 @@ public class GameplayGameStatsPanel : MonoBehaviour {
 #region -------------------- Serialized Variables --------------------
     [Header("Button Elements")]
 	[SerializeField] private SoM_Button _closeButton;
+	[SerializeField] private SoM_Button _returnButton;
+
+	[Header("Section Elements")]
+	[SerializeField] private List<TMP_Text> _homeTexts = new();
+	[SerializeField] private List<TMP_Text> _awayTexts = new();
+	[SerializeField] private List<Slider> _homeSliders = new();
+	[SerializeField] private List<Slider> _awaySliders = new();
 
     [Header("Main Elements")]
 	[SerializeField] private CanvasGroup _mainElement;
@@ -38,11 +45,14 @@ public class GameplayGameStatsPanel : MonoBehaviour {
     
 #endregion
 #region -------------------- Public Methods --------------------
-    public void InitializeMenuPanel()
+    public void InitializeGameStatsPanel()
 	{
         _mainElement.alpha = 0f;
 
         _closeButton.SetListener(() => { ClosePanel(); });
+		_returnButton.SetListener(() => { ClosePanel(); });
+
+		SetStats();
 
         AnimationController.Inst.FadeInPanel(_mainElement, _mainPanel, () =>
         {
@@ -62,6 +72,67 @@ public class GameplayGameStatsPanel : MonoBehaviour {
 	}
 #endregion
 #region -------------------- Private Methods --------------------
-    
+	private void SetStats()
+	{
+		int homeGoals = GameplayController.Inst.GameData.HomeTeam.Stats.Goals;
+		int homeShots = GameplayController.Inst.GameData.HomeTeam.Stats.Shots;
+		int homePPGs = GameplayController.Inst.GameData.HomeTeam.Stats.PowerplayGoals;
+		int homeSHGs = GameplayController.Inst.GameData.HomeTeam.Stats.ShorthandedGoals;
+		int homeFOWs = GameplayController.Inst.GameData.HomeTeam.Stats.FaceoffsWon;
+		int homeHits = GameplayController.Inst.GameData.HomeTeam.Stats.Hits;
+		int homeBSs = GameplayController.Inst.GameData.HomeTeam.Stats.BlockedShots;
+		int homeGAs = GameplayController.Inst.GameData.HomeTeam.Stats.Giveaways;
+		int homeTAs = GameplayController.Inst.GameData.HomeTeam.Stats.Takeaways;
+
+		int awayGoals = GameplayController.Inst.GameData.AwayTeam.Stats.Goals;
+		int awayShots = GameplayController.Inst.GameData.AwayTeam.Stats.Shots;
+		int awayPPGs = GameplayController.Inst.GameData.AwayTeam.Stats.PowerplayGoals;
+		int awaySHGs = GameplayController.Inst.GameData.AwayTeam.Stats.ShorthandedGoals;
+		int awayFOWs = GameplayController.Inst.GameData.AwayTeam.Stats.FaceoffsWon;
+		int awayHits = GameplayController.Inst.GameData.AwayTeam.Stats.Hits;
+		int awayBSs = GameplayController.Inst.GameData.AwayTeam.Stats.BlockedShots;
+		int awayGAs = GameplayController.Inst.GameData.AwayTeam.Stats.Giveaways;
+		int awayTAs = GameplayController.Inst.GameData.AwayTeam.Stats.Takeaways;
+
+		_homeTexts[0].text = homeGoals;
+		_homeTexts[1].text = homeShots;
+		_homeTexts[2].text = homePPGs;
+		_homeTexts[3].text = homeSHGs;
+		_homeTexts[4].text = homeFOWs;
+		_homeTexts[5].text = homeHits;
+		_homeTexts[6].text = homeBSs;
+		_homeTexts[7].text = homeGAs;
+		_homeTexts[8].text = homeTAs;
+
+		_awayTexts[0].text = awayGoals;
+		_awayTexts[1].text = awayShots;
+		_awayTexts[2].text = awayPPGs;
+		_awayTexts[3].text = awaySHGs;
+		_awayTexts[4].text = awayFOWs;
+		_awayTexts[5].text = awayHits;
+		_awayTexts[6].text = awayBSs;
+		_awayTexts[7].text = awayGAs;
+		_awayTexts[8].text = awayTAs;
+
+		_homeSliders[0].value = (homeGoals + awayGoals) == 0 ? 0 : homeGoals / (homeGoals + awayGoals);
+		_homeSliders[1].value = (homeShots + awayShots) == 0 ? 0 : homeShots / (homeShots + awayShots);
+		_homeSliders[2].value = (homePPGs + awayPPGs) == 0 ? 0 : homePPGs / (homePPGs + awayPPGs);
+		_homeSliders[3].value = (homeSHGs + awaySHGs) == 0 ? 0 : homeSHGs / (homeSHGs + awaySHGs);
+		_homeSliders[4].value = (homeFOWs + awayFOWs) == 0 ? 0 : homeFOWs / (homeFOWs + awayFOWs);
+		_homeSliders[5].value = (homeHits + awayHits) == 0 ? 0 : homeHits / (homeHits + awayHits);
+		_homeSliders[6].value = (homeBSs + awayBSs) == 0 ? 0 : homeBSs / (homeBSs + awayBSs);
+		_homeSliders[7].value = (homeGAs + awayGAs) == 0 ? 0 : homeGAs / (homeGAs + awayGAs);
+		_homeSliders[8].value = (homeTAs + awayTAs) == 0 ? 0 : homeTAs / (homeTAs + awayTAs);
+
+		_awaySliders[0].value = (homeGoals + awayGoals) == 0 ? 0 : awayGoals / (homeGoals + awayGoals);
+		_awaySliders[1].value = (homeShots + awayShots) == 0 ? 0 : awayShots / (homeShots + awayShots);
+		_awaySliders[2].value = (homePPGs + awayPPGs) == 0 ? 0 : awayPPGs / (homePPGs + awayPPGs);
+		_awaySliders[3].value = (homeSHGs + awaySHGs) == 0 ? 0 : awaySHGs / (homeSHGs + awaySHGs);
+		_awaySliders[4].value = (homeFOWs + awayFOWs) == 0 ? 0 : awayFOWs / (homeFOWs + awayFOWs);
+		_awaySliders[5].value = (homeHits + awayHits) == 0 ? 0 : awayHits / (homeHits + awayHits);
+		_awaySliders[6].value = (homeBSs + awayBSs) == 0 ? 0 : awayBSs / (homeBSs + awayBSs);
+		_awaySliders[7].value = (homeGAs + awayGAs) == 0 ? 0 : awayGAs / (homeGAs + awayGAs);
+		_awaySliders[8].value = (homeTAs + awayTAs) == 0 ? 0 : awayTAs / (homeTAs + awayTAs);
+	}
 #endregion
 }}
