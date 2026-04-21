8f38dc83f71fc05329acd9eadf33f3321bbfde34
The Ferryman <ferrymanstudios@gmail.com>
2026-04-21 11:44:10 -0500

    Create UiSeasonTeamStatistics.cs

---
+++ Scripts/Ui/Scenes/Season/UiSeasonTeamStatistics.cs
@@ -0,0 +1,45 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using SoM.Controllers;
+using SoM.Models;
+
+namespace SoM.Ui {
+public class UiSeasonTeamStatistics : UiSceneBase {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    void Start()
+    {
+        InitializeUi();
+    }
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    protected override void InitializeUi()
+	{
+        base.InitializeUi();
+	}
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
