22feeb4602a498d01515c20ef365b6d2ae7331b0
The Ferryman <ferrymanstudios@gmail.com>
2026-04-21 14:14:36 -0500

    Update UiSeasonNextGame.cs

--- Scripts/Ui/Scenes/Season/UiSeasonNextGame.cs
+++ Scripts/Ui/Scenes/Season/UiSeasonNextGame.cs
@@ -72,6 +72,10 @@ public class UiSeasonNextGame : UiSceneBase {
         List<Game> nightGames = new(SeasonsController.Inst.SeasonData.GameNights.FirstOrDefault(g => g.Number == night).Games);
 
         Game nextGame = nightGames.FirstOrDefault(ng => ng.HomeTeam.Team.Code == SeasonController.Inst.SeasonData.Team.Team.Code);
+
+        GameplayController.Inst.CreateSeasonGame();
+        GameplayController.Inst.GameData = nextGame;
+
         GameTeam nextTeam = nextGame.AwayTeam;
 
         string league = SeasonController.Inst.SeasonData.League.Contains("NHL") ? "NHL" : "PWHL";
@@ -93,28 +97,106 @@ public class UiSeasonNextGame : UiSceneBase {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the season edit lines screen.");
 
-        // TODO
+        GoToNewScene(CoreController.Inst.Scene_Season08);
     }
 
     private void GoToHome()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the home screen.");
 
-        // TODO
+        GoToNewScene(CoreController.Inst.Scene_Home00);
+    }
+
+    private void GoToSeasonStandings()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the season standings screen.");
+
+        GoToNewScene(CoreController.Inst.Scene_Season03);
+    }
+
+    private void GoToTeamStatistics()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the season team statistics screen.");
+
+        GoToNewScene(CoreController.Inst.Scene_Season04);
+    }
+
+    private void GoToSkaterStatistics()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the season skater statistics screen.");
+
+        GoToNewScene(CoreController.Inst.Scene_Season05);
+    }
+
+    private void GoToGoalieStatistics()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Going to the season goalie statistics screen.");
+
+        GoToNewScene(CoreController.Inst.Scene_Season06);
     }
 
     private void ShowDeletePanel()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Showing the delete season panel.");
 
-        // TODO
+        PanelController.Inst.ShowBottomPanel(ConstantController.PanelType.SeasonDeleteSeason, DeleteSeason);
     }
 
     private void ChangeNavigationOption(int option)
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Changing the season navigation view.");
 
-        // TODO
+        switch (option)
+		{
+			case 1:
+				GoToSeasonStandings();
+				break;
+			case 2:
+				GoToTeamStatistics();
+				break;
+            case 3:
+				GoToSkaterStatistics();
+				break;
+            case 4:
+				GoToGoalieStatistics();
+				break;
+			case 0:
+			default:
+                // Navigation is already Next Game
+				break;
+		}
     }
+
+    private async void DeleteSeason()
+	{
+		CoreController.Inst.WriteLog(this.GetType().Name, $"Deleting the user season.");
+
+		string userId = UsersController.Inst.UserData.Id;
+
+		PlayoffDatabase userPlayoffs = null;
+		SeasonDatabase userSeason = null;
+
+		await FirebaseController.Inst.GetPlayoffs(userId, async playoffs =>
+		{
+			userPlayoffs = playoffs;
+
+			await FirebaseController.Inst.GetSeason(userId, season =>
+			{
+				userSeason = season;
+			});
+		});
+
+		if (userPlayoffs != null)
+		{
+			await FirebaseController.Inst.DeletePlayoffs(userId);
+		}
+
+		if (userSeason != null)
+		{
+			await FirebaseController.Inst.DeleteSeason(userId);
+		}
+
+        GoToHome();
+	}
 #endregion
 }}
