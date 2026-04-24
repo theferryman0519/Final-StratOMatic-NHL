cf275342a479706b6398c33cd3ca678f80c4fb6e
The Ferryman <ferrymanstudios@gmail.com>
2026-04-24 09:36:31 -0500

    Update PlayoffsController.cs

--- Scripts/Controllers/PlayoffsController.cs
+++ Scripts/Controllers/PlayoffsController.cs
@@ -16,7 +16,8 @@ namespace SoM.Controllers {
 public class PlayoffsController : Singleton<PlayoffsController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Creation Elements")]
+    [SerializeField] private PlayoffCreation _playoffCreation;
 #endregion
 #region -------------------- Public Variables --------------------
     public Playoff PlayoffData;
@@ -35,9 +36,38 @@ public class PlayoffsController : Singleton<PlayoffsController> {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the controller.");
         
-        CoreController.Inst.LoadingStepCompleted();
+        LoadPlayoffData();
+    }
+
+    public async Task CreateNewPlayoff(string team, string league, Action continueAction = null)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Creating a new playoff.");
+
+        PlayoffData = null;
+
+        PLayoffDatabase newPlayoffDatabase = new PLayoffDatabase
+        {
+            Id = Guid.NewGuid().ToString(),
+            League = league,
+            Team = team,
+            Round = 1,
+            GameNumber = 1,
+            SkaterLineup = new(),
+            GoalieLineup = new(),
+        };
+
+        PlayoffData = await _playoffCreation.CreatePlayoff(newPlayoffDatabase);
+
+        continueAction?.Invoke();
+    }
+
+    public void LoadCurrentPlayoff(Action continueAction = null)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Loading the current playoff.");
 
         // TODO
+
+        continueAction?.Invoke();
     }
 
     public TeamPlayoff GetTeamPlayoff(GameTeam team)
@@ -60,6 +90,55 @@ public class PlayoffsController : Singleton<PlayoffsController> {
     }
 #endregion
 #region -------------------- Private Methods --------------------
-    
+    private async void LoadPlayoffData()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Loading the playoff data.");
+
+        await FirebaseController.Inst.GetPlayoff(UsersController.Inst.UserData.Id, async playoffData =>
+        {
+            PlayoffData = null;
+        
+            if (playoffData != null)
+            {
+                PlayoffData = new Playoff
+                {
+                    Id = playoffData.Id,
+                    League = playoffData.League,
+                    CurrentRound = playoffData.Round,
+                    Rounds = new(),
+                };
+
+                // TODO: Set Rounds
+        
+                ConstantController.LeagueType leagueType = ConstantController.LeagueType.None;
+        
+                if (PlayoffData.League == "NHL") { leagueType = ConstantController.LeagueType.NHL; }
+                else { leagueType = ConstantController.LeagueType.PWHL; }
+        
+                Team userTeam = TeamsController.Inst.GetTeamFromCode(seasonData.Team, leagueType);
+        
+                GameTeam userGameTeam = new GameTeam
+                {
+                    SkaterLineup = new(),
+                    GoalieLineup = new(),
+                    CurrentLine = 1,
+                    CurrentPair = 1,
+                    CurrentStrategy = 3,
+                    NextLine = 1,
+                    NextPair = 1,
+                    NextStrategy = 3,
+                    IsGoaliePulled = false,
+                    Team = userTeam.Info,
+                    Stats = userTeam.Game,
+                };
+        
+                PlayoffData.Team = userGameTeam;
+                PlayoffData.Team.SkaterLineup = await SetSkaterLineup(seasonData.SkaterLineup);
+                PlayoffData.Team.GoalieLineup = await SetGoalieLineup(seasonData.GoalieLineup);
+            }
+        
+            CoreController.Inst.LoadingStepCompleted();
+        });
+    }
 #endregion
 }}
